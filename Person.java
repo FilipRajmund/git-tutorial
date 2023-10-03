5a4c2cefcb4bba829b0e1a58c916c97ad02fb641
filip <filip1@gamial.pl>
2023-10-03 12:27:28 +0200

    new commit 3

--- Person.java
+++ Person.java
@@ -4,6 +4,9 @@ public class Person {
     private String surName;
     private int age;
 
-
-
+    public Person(String name, String surName, int age) {
+        this.name = name;
+        this.surName = surName;
+        this.age = age;
+    }
 }
