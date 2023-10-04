4c1169c97165f8b5f3fc06511237573084822198
filip <filip1@gamial.pl>
2023-10-04 16:06:57 +0200

    commit file 6 main

--- Person.java
+++ Person.java
@@ -27,5 +27,16 @@ public class Person {
         this.surName = surName;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name) && Objects.equals(surName, person.surName);
+    }
 
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, surName, age);
+    }
 }
