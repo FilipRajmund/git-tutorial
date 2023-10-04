7489b545fd13bea2575b48bab2adc7753f25be58
filip <filip1@gamial.pl>
2023-10-04 14:09:17 +0200

    Person4

--- Person.java
+++ Person.java
@@ -9,4 +9,20 @@ public class Person {
         this.surName = surName;
         this.age = age;
     }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurName() {
+        return surName;
+    }
+
+    public void setSurName(String surName) {
+        this.surName = surName;
+    }
 }
